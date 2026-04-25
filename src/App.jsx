65a492d8b4e0b099b33e044d0b9e68282113544c
suvo753 <mdshuvoislamsams795@gmail.com>
2026-04-25 19:52:06 +0600

    Navbar And Banner Design Comnplate

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+import './App.css'
+import Banner from './components/Banner/Banner'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+
+
+  return (
+
+
+
+    <div>
+
+      <Navbar/>
+      <Banner/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+  )
+}
+
+export default App
